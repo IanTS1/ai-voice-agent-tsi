2280162870374d8848a1c55803f60ce2a1703142
IanTS1 <ian.ramirez@trisourcebpo.com>
2024-06-11 11:43:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def get_assistant_instructions():
 # GPT4AllAgent and ChatAnthropicAgent.
 # Don't forget to set OPENAI_API_KEY!
 AGENT_CONFIG = ChatGPTAgentConfig(
-  initial_message=BaseMessage(text="Hello, who are you?"),
+  initial_message=BaseMessage(text="Hello, who are you? and why are you calling?"),
   prompt_preamble=get_assistant_instructions(),
   generate_responses=True,
 )
